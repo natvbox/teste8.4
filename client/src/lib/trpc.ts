929250cd1c41e7dc112b29b57ab7e1849d5d10f0
natvbox <ninfoboyn@gmail.com>
2026-02-13 15:04:11 -0300

    Update trpc.ts

--- client/src/lib/trpc.ts
+++ client/src/lib/trpc.ts
@@ -1,4 +1,12 @@
 import { createTRPCReact } from "@trpc/react-query";
 import type { AppRouter } from "../../../server/routers";
 
+/**
+ * Hook principal do tRPC para React
+ * Usado em todo o frontend.
+ *
+ * Ex:
+ * const { data } = trpc.notifications.list.useQuery(...)
+ */
 export const trpc = createTRPCReact<AppRouter>();
+
